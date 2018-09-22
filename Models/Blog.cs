9ff5eeef348a231bdbfe30abbb4d6449dda69fec
User 2 <user-2@example.com>
2018-09-22 16:29:13 +0200

    Added readers to blog

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -13,6 +13,8 @@ namespace EfCoreMigrations.Models
 
         public string Author { get; set; }
 
+        public int Readers { get; set; }
+
         public virtual ICollection<Post> Posts { get; set; }      
     }
 }
